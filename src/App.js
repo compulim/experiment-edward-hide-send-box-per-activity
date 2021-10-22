c7cc588d2c70575fdf9c8dcf50aec17c5f8a27ea
William Wong <compulim@users.noreply.github.com>
2021-10-22 21:20:10 +0000

    Clean up

--- src/App.js
+++ src/App.js
@@ -3,27 +3,25 @@ import './App.css';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import ReactWebChat, { createDirectLine, createStore } from 'botframework-webchat';
 
+const MESSAGE_QUOTA = 5;
+
 function App() {
   const [numMessage, setNumMessage] = useState(0);
   const [token, setToken] = useState();
 
   const directLine = useMemo(() => token && createDirectLine({ token }), [token]);
   const increment = useCallback(() => setNumMessage(numMessage => numMessage + 1), [setNumMessage]);
-  const shouldHideSendBox = useMemo(() => numMessage >= 5, [numMessage]);
+  const shouldHideSendBox = useMemo(() => numMessage >= MESSAGE_QUOTA, [numMessage]);
 
-  const store = useMemo(() => {
-    const store = createStore({}, () => next => action => {
-      console.log(action);
+  const store = useMemo(
+    () =>
+      createStore({}, () => next => action => {
+        action.type === 'DIRECT_LINE/INCOMING_ACTIVITY' && increment();
 
-      if (action.type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
-        increment();
-      }
-
-      return next(action);
-    });
-
-    return store;
-  }, [increment]);
+        return next(action);
+      }),
+    [increment]
+  );
 
   const styleOptions = useMemo(() => ({ hideSendBox: shouldHideSendBox }), [shouldHideSendBox]);
 
@@ -31,7 +29,10 @@ function App() {
     const abortController = new AbortController();
 
     (async function () {
-      const res = await fetch('https://webchat-mockbot.azurewebsites.net/directline/token', { method: 'POST' });
+      const res = await fetch('https://webchat-mockbot.azurewebsites.net/directline/token', {
+        method: 'POST',
+        signal: abortController.signal
+      });
 
       if (!res.ok) {
         throw new Error('Failed to fetch token.');
@@ -48,7 +49,7 @@ function App() {
   return (
     <div className="app">
       {!!directLine && <ReactWebChat directLine={directLine} store={store} styleOptions={styleOptions} />}
-      <div className="app__counter">You have {Math.max(0, 5 - numMessage)} messages left.</div>
+      <div className="app__counter">You have {Math.max(0, MESSAGE_QUOTA - numMessage)} messages left.</div>
     </div>
   );
 }
